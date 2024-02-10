7b8a743ecc4fdc91e66971b927e2ce675ab3aaae
Oliwia <106619574+okrzanowska@users.noreply.github.com>
2024-02-10 13:18:52 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace PetlaWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int minimum = int.MaxValue;
+
+            Console.WriteLine("Podaj liczby od 0 do 10, aby zatrzymać pętlę podaj liczbę 5.");
+
+            while (true)
+            {
+
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                if (number == 5)
+                {
+                    break;
+                }
+
+                if (number < minimum)
+                {
+                    minimum = number;
+                }
+
+                sum += number;
+            }
+
+            sum = sum * sum;
+
+            Console.WriteLine("Suma wszystkich elementów podniesiona do kwadratu: " + sum);
+            Console.WriteLine("Najmniejszy element: " + minimum);
+        }
+    }
+}
